67879314be037eaa2a2c1cb8204156ee3771ff21
Adrian <adrianco2111@gmail.com>
2023-03-02 07:01:21 -0500

    Modificacion en config

--- config/database.js
+++ config/database.js
@@ -6,8 +6,8 @@ const db = "dasansor";
 const BASE_URL = process.env.BASE_URL;
 
 exports.mongoConnect = () => {
-    const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
-
+   // const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+   const mongoStringConnection = `mongodb://$:{BASE_URL}${host}:${port}/${db}`;
     mongoose.connect(mongoStringConnection);
     mongoose.Promise = global.Promise;
     const dbConnection = mongoose.connection;
