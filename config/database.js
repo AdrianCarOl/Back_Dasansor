dd1cd82774c39a805943009f3513a5706fa3753a
Adrian <adrianco2111@gmail.com>
2023-03-08 20:21:16 -0500

    Update 8MAR2023

--- config/database.js
+++ config/database.js
@@ -5,7 +5,8 @@ const port = "27017";
 const db = "dasansor";
 
 exports.mongoConnect = () => {
-    const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+    //const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+    const mongoStringConnection = `mongo "mongodb://mongo:PKo7F0i3nCipiVQsZ8G0@containers-us-west-23.railway.app:5733"`;
 
     mongoose.connect(mongoStringConnection);
     mongoose.Promise = global.Promise;
